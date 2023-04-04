a093185f10724e303cfe1f38f971abebd8c6a006
GabrielHLock <gabrielhendrick@outlook.com>
2023-04-04 10:34:01 -0400

    modified:   artport.js 	new file:   public/footer.css 	new file:   public/main_div.css 	deleted:    public/style.css 	new file:   public/test_headercss.css 	new file:   views/main.ejs 	modified:   views/partials/footer.ejs 	modified:   views/partials/header.ejs

--- artport.js
+++ artport.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs")
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>
-	//res.render('first',{data:data})
+	res.render('main')
 )
 
 app.listen(8080,()=>console.log("server running"))
