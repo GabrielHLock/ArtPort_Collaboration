003ce6739bac153d9c068b6b9f2e9b47403a67b8
NathArcher <nathanielarcher@outlook.com>
2023-04-10 10:01:45 -0400

    updated

--- testUpload.js
+++ testUpload.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const app = express();
-const port = 5060;
+const port = 3000;
 const fs = require('fs');
 
+var bodyParser = require('body-parser');
+
 app.use(express.static('public'));
-app.use(express.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 const jsonData = fs.readFileSync('data/submissions.json');
 let submissionsData = JSON.parse(jsonData).users;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/testUpload.ejs');
-});
+app.set("view engine","ejs")
+app.get ('/',(req,res)=>
+res.render('testUpload'))
 
 app.post('/submit', (req, res) => {
-  const { username, title, image, description } = req.body;
+  res.send(req.body);
+  // const { username, title, image, description } = req.body;
 
   // Check if the username already exists in the submissions data
-  const user = submissionsData.find(user => user.username === username);
+//   const user = submissionsData.find(user => user.username === username);
+// console.log(user, username);
+//   if (!user) {
+//     return res.status(404).send('This username does not exist. Please create a new account or sign in if you already have one.');
+//   }
 
-  if (!user) {
-    return res.status(404).send('This username does not exist. Please create a new account or sign in if you already have one.');
-  }
+//   // Add new submission to the user's submissions and save to file
+//   user.submissions.push({ title, image, description });
+//   fs.writeFileSync('submissions.json', JSON.stringify({ users: submissionsData }));
 
-  // Add new submission to the user's submissions and save to file
-  user.submissions.push({ title, image, description });
-  fs.writeFileSync('submissions.json', JSON.stringify({ users: submissionsData }));
-
-  res.send('Submission added successfully!');
+//   res.send('Submission added successfully!');
 });
 
 app.listen(port, () => {
